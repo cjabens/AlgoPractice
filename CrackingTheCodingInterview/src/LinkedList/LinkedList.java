351ddbf125f2cddcd424493f7e1144cf09595b8a
Joshua MacVey <joshua.macvey@target.com>
2019-11-21 22:24:30 -0600

    added linked list

---
+++ CrackingTheCodingInterview/src/LinkedList/LinkedList.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Node {
+    private String value;
+    private Node next;
+
+    Node(String value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+};
